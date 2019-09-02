40877f01cf6fea38c389d30a4dcc8d68a1ee787c
Donald Freeman <freemandonaldc@gmail.com>
2019-09-02 12:54:29 -0400

    Add basic validation around k input

--- src/main/ArraySolver.java
+++ src/main/ArraySolver.java
@@ -1,4 +1,16 @@
 package main;
 
+import java.util.List;
+
 public class ArraySolver {
+    public List<Double> solve(Double[] inputArray, double x, int k) {
+        validate(k);
+        return null;
+    }
+
+    private void validate(int k) {
+        if(k<1) {
+            throw new IllegalArgumentException("k must be a positive integer.");
+        }
+    }
 }
