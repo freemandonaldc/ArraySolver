4c70acaa3f66ffcee9adf4579bb11c271e8a2695
Donald Freeman <freemandonaldc@gmail.com>
2019-09-02 14:01:05 -0400

    Clean up formatting

--- src/main/ArraySolver.java
+++ src/main/ArraySolver.java
@@ -7,17 +7,14 @@ import java.util.List;
 public class ArraySolver {
     public List<Double> solve(Double[] inputArray, double x, int k) {
         validate(k);
-
         List<Double> resultArrayList = new ArrayList<>();
-
-        for(int iteration = 0; iteration < k; iteration++) {
-            if(inputArray.length <=0) {
+        for (int iteration = 0; iteration < k; iteration++) {
+            if (inputArray.length <= 0) {
                 break;
             }
 
             Double lowestDistanceFromX = Math.abs(inputArray[0] - x);
             int bestIndex = 0;
-
             for (int i = 0; i < inputArray.length; i++) {
                 Double currentDistanceFromX = Math.abs(inputArray[i] - x);
                 if (currentDistanceFromX < lowestDistanceFromX) {
@@ -25,6 +22,7 @@ public class ArraySolver {
                     bestIndex = i;
                 }
             }
+
             resultArrayList.add(inputArray[bestIndex]);
             inputArray = removeIndexFromArray(inputArray, bestIndex);
         }
@@ -35,17 +33,17 @@ public class ArraySolver {
 
     private Double[] removeIndexFromArray(Double[] inputArray, int index) {
         ArrayList<Double> newValues = new ArrayList<>();
-        for(int i=0; i<inputArray.length; i++) {
-            if(i!=index) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if (i != index) {
                 newValues.add(inputArray[i]);
             }
         }
 
-        return newValues.toArray(new Double[inputArray.length-1]);
+        return newValues.toArray(new Double[inputArray.length - 1]);
     }
 
     private void validate(int k) {
-        if(k<1) {
+        if (k < 1) {
             throw new IllegalArgumentException("k must be a positive integer.");
         }
     }
