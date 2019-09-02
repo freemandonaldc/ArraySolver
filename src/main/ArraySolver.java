ab7864b9ab06e25fce105dfa403db953e6fdb191
Donald Freeman <freemandonaldc@gmail.com>
2019-09-02 13:48:07 -0400

    Implement core sover logic

--- src/main/ArraySolver.java
+++ src/main/ArraySolver.java
@@ -1,11 +1,41 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArraySolver {
     public List<Double> solve(Double[] inputArray, double x, int k) {
         validate(k);
-        return null;
+
+        List<Double> resultArrayList = new ArrayList<>();
+
+        for(int iteration = 0; iteration < k; iteration++) {
+            Double lowestDistanceFromX = Math.abs(inputArray[0] - x);
+            int bestIndex = 0;
+
+            for (int i = 0; i < inputArray.length; i++) {
+                Double currentDistanceFromX = Math.abs(inputArray[i] - x);
+                if (currentDistanceFromX < lowestDistanceFromX) {
+                    lowestDistanceFromX = currentDistanceFromX;
+                    bestIndex = i;
+                }
+            }
+            resultArrayList.add(inputArray[bestIndex]);
+            inputArray = removeIndexFromArray(inputArray, bestIndex);
+        }
+
+        return resultArrayList;
+    }
+
+    private Double[] removeIndexFromArray(Double[] inputArray, int index) {
+        ArrayList<Double> newValues = new ArrayList<>();
+        for(int i=0; i<inputArray.length; i++) {
+            if(i!=index) {
+                newValues.add(inputArray[i]);
+            }
+        }
+
+        return newValues.toArray(new Double[inputArray.length-1]);
     }
 
     private void validate(int k) {
